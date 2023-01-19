e0ed2c0efffd3479f60a828b978145e126964936
Kawtar SETTAR <kawtar.settar@imerir.com>
2023-01-19 10:07:39 +0100

    update code

--- test.py
+++ test.py
@@ -1,45 +1,121 @@
 import nltk
 import spacy
 from nltk import word_tokenize
+from nltk.corpus import stopwords
+import pandas as pd
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+from nltk.stem.snowball import FrenchStemmer
+from nltk.tokenize import word_tokenize
+from spacy.lang.fr.stop_words import STOP_WORDS
+from spacy.tokens import token
+import string
+import pandas as pd
+import json
 
 nltk.download('wordnet')
 nltk.download('punkt')
 nltk.download('stopwords')
+nltk.download('omw-1.4')
 
 # Chargement du modèle en français de Spacy
 nlp = spacy.load("fr_core_news_sm")
 
+
+df_family = pd.read_csv('/home/settar/Bureau/UGOFresh/ai4industry-ugofresh/DataCSV/Famille.csv')
+df_subfamily = pd.read_csv('/home/settar/Bureau/UGOFresh/ai4industry-ugofresh/DataCSV/Sous-famille.csv')
+df_variety = pd.read_csv('/home/settar/Bureau/UGOFresh/ai4industry-ugofresh/DataCSV/Variété.csv')
+df_origin = pd.read_csv('/home/settar/Bureau/UGOFresh/ai4industry-ugofresh/DataCSV/Pays.csv')
+df_packaging = pd.read_csv('/home/settar/Bureau/UGOFresh/ai4industry-ugofresh/DataCSV/Conditionnement.csv')
+df_quantity = pd.read_csv('/home/settar/Bureau/UGOFresh/ai4industry-ugofresh/DataCSV/Quantité.csv')
+df_calibre = pd.read_csv('/home/settar/Bureau/UGOFresh/ai4industry-ugofresh/DataCSV/Calibre.csv')
+df_labels = pd.read_csv('/home/settar/Bureau/UGOFresh/ai4industry-ugofresh/DataCSV/Labels.csv')
+
+def tokenize(sentence):
+  for token in sentence:
+    return [token.text for token in sentence]
+
+def lemma(sentence):
+    return [token.lemma_ for token in nlp(" ".join(sentence))]
+
+def removeStopWords(sentence):
+  return [word for word in tokenize(sentence) if not word in STOP_WORDS]
+
+def removePonct(sentence):
+  return [word for word in sentence if word not in string.punctuation]
+
+def traitement(sentence):
+    onesentence=nlp(sentence)
+    newsentence=nlp(onesentence)
+    removed_stpwrds= removeStopWords(newsentence)
+    removed_ponct = removePonct(removed_stpwrds)
+    lemmatized_sentence = lemma(removed_ponct)
+    return lemmatized_sentence
+  
+
+
+def csv_to_json(csv_file, json_file):
+    # lire le fichier csv en utilisant pandas
+    df = pd.read_csv(csv_file)
+    # créer un dictionnaire vide
+    data = {}
+    # itérer sur les lignes du fichier csv
+    for index, row in df.iterrows():
+        # ajouter une entrée au dictionnaire avec la valeur de la colonne Valeur comme clé et la valeur de la colonne Texte comme valeur
+        data[row['Valeur']] = row['Texte']
+    
+    print(data)
+    # écrire le dictionnaire dans un fichier json
+    '''with open(json_file, 'w') as f:
+        json.dump(data, f)
+
+'''
+
 def extract_info(sentence):
-    # Tokenisation de la phrase
-    tokens = word_tokenize(sentence)
-    # Analyse de la phrase avec Spacy
-    doc = nlp(" ".join(tokens))
-    famille = ""
-    conditionnement = ""
-    sous_famille = ""
-    variete = ""
-    origine = ""
-    quantite = ""
-    unite = ""
-    for token in doc:
-        if token.pos_ == "NOUN":
-            if token.text.lower() in ["aubergine", "aubergines"]:
-                famille = "aubergine"
-            elif token.text.lower() in ["cagette", "cagettes"]:
-                conditionnement = "cagette"
-            elif token.text.lower() in ["grafiti"]:
-                sous_famille = "grafiti"
-            elif token.text.lower() in ["angela"]:
-                variete = "angela"
-            elif token.text.lower() in ["france", "francais", "francaise"]:
-                origine = "France"
-        elif token.like_num:
-            quantite = token.text
-        elif token.text.lower() in ["kg", "kilogramme", "kilogrammes"]:
-            unite = "kilogramme"
-    return {"famille": famille, "conditionnement": conditionnement, "sous_famille": sous_famille, "variete": variete, "origine": origine, "quantite": quantite, "unite": unite}
+    
+    df_subfamily=csv_to_json('/home/settar/Bureau/UGOFresh/ai4industry-ugofresh/DataCSV/Sous-famille.csv', 'Sous-famille.json')
+
+    with open('Sous-famille.json') as json_file:
+        data = json.load(json_file, )
+        print(data["Texte"])
+        for sentence in data['Texte']:
+            lemmatized_sentence = traitement(sentence)
+            print(lemmatized_sentence)
+
+
+
+
+    # doc = nlp(" ".join(traitement(sentence)))
+    # print(doc)
+
+    # famille = ""
+    # conditionnement = ""
+    # sous_famille = ""
+    # variete = ""
+    # origine = ""
+    # quantite = ""
+    # unite = ""
+
+    # for token in doc:
+    #     if token.pos_ == "NOUN":
+    #         if token.text.lower() in df_subfamily["Texte"].str.lower():  
+    #             sous_famille = df_subfamily["Valeur"]["Texte" == token.text.lower()]
+    #             print(famille)
+    #         elif token.text.lower() in df_packaging["Synonymes"].str.lower():
+    #             conditionnement = df_packaging["Valeur"]["Synonymes" == token.text.lower()]
+    #         elif token.text.lower() in df_variety["Texte"].str.lower():
+    #             variete = df_variety["Valeur"]["Texte" == token.text.lower()]
+    #         elif token.text.lower() in df_origin["Texte"].str.lower():
+    #             origine = df_origin["Valeur"]["Texte" == token.text.lower()]
+    #     elif token.like_num:
+    #         quantite = token.text
+    #     elif token.text.lower() in ["kg", "kilogramme", "kilogrammes"]:
+    #         unite = "kilogramme"
+    # return {"famille": famille, "conditionnement": conditionnement, "sous_famille": sous_famille, "variete": variete, "origine": origine, "quantite": quantite, "unite": unite}
 
 
 sentence = "J'ai 300kg d'aubergines type grafiti variété angela, en cagette qui viennent de France."
-info = extract_info(sentence)
-print(info)
+print(traitement(sentence))
+#info = extract_info(sentence)
+
+
